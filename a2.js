bbd84826f6b8c7a7b7db2370433aa698eafcf832
khushi1711 <90022273+khushi1711@users.noreply.github.com>
2021-11-01 10:23:33 +0530

    Add files via upload

---
+++ a2.js
@@ -0,0 +1,3 @@
+var x="Khushi Kumari";
+x=17;
+x=true;
---
+++ que3.js
@@ -0,0 +1,12 @@
+
+console.log(x);
+if(x==undefined){
+    console.log("x is undefined");
+}
+
+if(x==undefined){
+    console.log("x is undefned");
+}
+else{
+    console.log("x has been defined");
+}
